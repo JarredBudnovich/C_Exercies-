9034234bed0c15b9c5ab5942e908cadf84d489d0
jabbitrock <jbudnovich@gmail.com>
2022-08-10 22:39:28 -0400

    Exercise 1-3 add header to table output

--- Chapter_1/1-3.c
+++ Chapter_1/1-3.c
@@ -7,14 +7,15 @@ int main()
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;  //lower limit of tmperature table
+    lower = 0;  //lower limit of temperature table
     upper = 300; //upper limit
     step = 20;   //step size
 
     fahr = lower;
+    printf("Fahrenheit      Celsius\n");
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%-15.0f %-10.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
