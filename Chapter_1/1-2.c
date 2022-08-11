c721fdf6642338f01068f9ed1d860c24e2bf2e45
jabbitrock <jbudnovich@gmail.com>
2022-08-10 21:52:01 -0400

    New exercise working off of 1-1.c to change \n to \v to see results

---
+++ Chapter_1/1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Simple Hello, World! exercise to see what happens when printf's argument string contains \v
+
+int main() 
+{
+    printf("Hello, World! \v");
+
+    return 0;
+}
